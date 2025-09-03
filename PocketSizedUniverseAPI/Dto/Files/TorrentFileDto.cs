60f6297f8c78f8436293cfd35cecc3a8cdbfb430
NostraThomas99 <34015422+NostraThomas99@users.noreply.github.com>
2025-09-03 00:17:45 -0500

    Add POST magnet link endpoint route

--- PocketSizedUniverseAPI/Dto/Files/TorrentFileDto.cs
+++ PocketSizedUniverseAPI/Dto/Files/TorrentFileDto.cs
@@ -3,13 +3,11 @@
 namespace PocketSizedUniverse.API.Dto.Files;
 
 [MessagePackObject(keyAsPropertyName: true)]
-public record DownloadFileDto : ITransferFileDto
+public record TorrentFileDto : ITransferFileDto
 {
     public bool FileExists { get; set; } = true;
     public string Hash { get; set; } = string.Empty;
-    public string Url { get; set; } = string.Empty;
-    public long Size { get; set; } = 0;
     public bool IsForbidden { get; set; } = false;
     public string ForbiddenBy { get; set; } = string.Empty;
-    public long RawSize { get; set; } = 0;
+    public string MagnetLink { get; set; } = string.Empty;
 }
